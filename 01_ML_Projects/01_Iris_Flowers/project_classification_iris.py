4c2c171ecf43ff618437650c2937aaca0687eeeb
neurathon <93470347+neurathon@users.noreply.github.com>
2021-12-20 13:19:51 +0100

    Update project_classification_iris.py

--- 01_ML_Projects/01_Iris_Flowers/project_classification_iris.py
+++ 01_ML_Projects/01_Iris_Flowers/project_classification_iris.py
@@ -68,6 +68,13 @@ pyplot.show()
 #   • Cleaning data by removing duplicates, marking missing values and even imputing missing values.
 #   • Feature selection where redundant features may be removed and new features developed.
 #   • Data transforms where attributes are scaled or redistributed in order to best expose the structure of the problem later to learning algorithms.
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=seed)
 # --------------------------------------------------------------------------------------------
 # 4. Evaluate Algorithms
 # a) Split-out validation dataset
